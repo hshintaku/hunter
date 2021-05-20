2523a0c74b6844aeed6911d8b12d0757e0e62fef
hirofumi shintaku <hirofumi.shintaku@riken.jp>
2021-05-20 14:20:00 +0900

    separate the first data processing

---
+++ hunter_first_data_process.R
@@ -0,0 +1,27 @@
+# load functions for barcode decoding
+source(file.path(rdir,"whitelist_encode.R"))
+# laod whitelist and check the batch effect
+source(file.path(rdir,'hunter_preprocess_whitelist.R'))
+
+# preprocess the count data and load reference
+source(file.path(rdir,'hunter_preprocess_data.R'))
+#
+# download reference data from ensembl with biomaRt
+gene_list <- unique(data.frame(str_replace(allData$gene,"_intron","")))
+colnames(gene_list) <- "gene"
+source(file.path(rdir,'hunter_biomart_ref.R'))
+#hs_ref <- func.biomart.ref(hs_mart,gene_list,"hgnc_symbol")
+filter="ensembl_gene_id"
+#symbol="mgi_symbol"
+symbol="hgnc_symbol"
+hs_mart <- useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl")
+#ms_mart <- useMart(biomart="ensembl", dataset="mmusculus_gene_ensembl")
+#pig_mart <- useMart(biomart="ensembl", dataset="sscrofa_gene_ensembl")
+ms_ref <- unique(func.biomart.ref(hs_mart,gene_list,filter,symbol))
+missing_ref <- subset(gene_list,!(gene %in% ms_ref$ensembl_gene_id))
+adding_ref <- data.frame(cbind(missing_ref$gene,missing_ref$gene,missing_ref$gene,missing_ref$gene,missing_ref$gene))
+colnames(adding_ref) <- colnames(ms_ref)
+rownames(adding_ref) <- adding_ref$ensembl_gene_id
+ms_ref <- rbind(adding_ref,ms_ref)
+# save count data with 10x format
+source(file.path(rdir, 'hunter_preprocess_save_10x_format.R'))
