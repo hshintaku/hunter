9ef463088cc86618559bd77787cd5eda282e836d
hirofumi shintaku <hirofumi.shintaku@riken.jp>
2021-06-05 07:52:07 +0900

    added ggplot to visualize the correlation with a ascatter

--- io/hunter_Seurat_load_fld_data.R
+++ io/hunter_Seurat_load_fld_data.R
@@ -3,7 +3,7 @@ library(stringr)
 library(dplyr)
 
 #source(file.path(rdir,"shiomi_preprocess_FLD_data.R"))
-FLDmapALL <- load.fld(datadir,"umi_count",barcode)
+FLDmapALL <- load.fld(datadir,"read_count",barcode)
 colnames(FLDmapALL) <- c("romin","FLD004","FLD010","FLD070","FLD500","FLDcon","Unmapped","GC")
 rownames(FLDmapALL) <- toupper(rownames(FLDmapALL))
 
@@ -26,7 +26,11 @@ colnames(FLDcomb) <- c("FLD004","FLD010","FLD070","FLD500","Venus")
 #FLDcomb$GC <- FLDsub[,]$GC
 #ggplot(FLDcomb,aes(x=Venus,y=FLD070,color=GC))+geom_point()+scale_x_log10(limits=c(1,1e5))+scale_y_log10(limits=c(1,1e5))
 
+FeatureScatter(pbmc,feature1="Venus",feature2="FLD500")+scale_x_log10()+scale_y_log10()
+
+FLDmelt <- melt(FLDcomb,id.vars = "Venus")
+ggplot(FLDmelt,aes(x=Venus+1,y=value+1,color=variable))+geom_point()+scale_x_log10()+scale_y_log10()
 cor(FLDcomb, method="spearman")
 
-rm(seladt.FLD.csv,pbmc.adt.FLD,FLDcomb)
+rm(seladt.FLD.csv,pbmc.adt.FLD,FLDcomb,FLDmelt)
 
