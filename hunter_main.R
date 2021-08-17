dcc47f04886560889aab473fb89ee4d21fa54ffa
Hirofumi Shintaku <hirofumi.shintaku@riken.jp>
2021-08-17 09:08:06 +0900

    update for 10x using higher dimension

--- hunter_main.R
+++ hunter_main.R
@@ -60,6 +60,20 @@ source(file.path(rdir,'/io/hunter_Seurat_load_adt_data.R'))
 #
 source(file.path(rdir,'io/hunter_Seurat_load_fld_data.R'))
 #
+#pbmc.adt.FLD <-as.sparse(t(seladt.FLD.csv)) # convert the format to sparse matrix
+source(file.path(rdir,'20210816HiSeqX004_annotate_condition.R'))
+pbmc[["FLD"]] <- CreateAssayObject(counts=new_pbmc.tag[c("FLD004","FLD010","FLD070","FLD150","FLD500","FLDtotal"),])
+
+source(file.path(rdir,'20210816HiSeqX004_visualize_condition.R'))
+#pbmc <- NormalizeData(pbmc, normalization.method = "CLR", margin=2,assay = "FLD")
+pbmc[["HTO"]] <- CreateAssayObject(counts=pbmc.tag[c("T20CTL","T50CTL","TAZCTL"),])
+pbmc <- NormalizeData(pbmc, normalization.method = "CLR", scale.factor = 1e5,assay = "HTO")
+
+DimPlot(pbmc,reduction="pca")
+#p1<-DimPlot(pbmc,reduction="umap")
+FeaturePlot(pbmc,features=c("FLD500","T20CTL","T50CTL","TAZCTL"),reduction = "pca")
+
+#
 source(file.path(rdir,"shiomi_fld_external_control_analysis.R"))
 # first overview
 # analyze data with PCA and UMAP
--- io/hunter_Seurat_load_fld_data.R
+++ io/hunter_Seurat_load_fld_data.R
@@ -23,19 +23,6 @@ pbmc.tag[,selcellids]<-seladt.FLD.csv[,selcellids]
 rm(cellids_fld,selcellids)
 #seladt.FLD.csv[is.na(seladt.FLD.csv$romin),]<-0
 
-#pbmc.adt.FLD <-as.sparse(t(seladt.FLD.csv)) # convert the format to sparse matrix
-source(file.path(rdir,'20210816HiSeqX004_annotate_condition.R'))
-pbmc[["FLD"]] <- CreateAssayObject(counts=new_pbmc.tag[c("FLD004","FLD010","FLD070","FLD150","FLD500","FLDtotal"),])
-source(file.path(rdir,'20210816HiSeqX004_visualize_condition.R'))
-
-
-#pbmc <- NormalizeData(pbmc, normalization.method = "CLR", margin=2,assay = "FLD")
-pbmc[["HTO"]] <- CreateAssayObject(counts=pbmc.tag[c("T20CTL","T50CTL","TAZCTL"),])
-pbmc <- NormalizeData(pbmc, normalization.method = "CLR", scale.factor = 1e5,assay = "HTO")
-
-DimPlot(pbmc,reduction="pca")
-#p1<-DimPlot(pbmc,reduction="umap")
-FeaturePlot(pbmc,features=c("FLD500","T20CTL","T50CTL","TAZCTL"),reduction = "pca")
 
 
 rm(seladt.FLD.csv,pbmc.adt.FLD,FLDcomb,FLDmelt)
