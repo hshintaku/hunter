3af2548217246b5a26313151e807a8da56033046
hshintaku <hirofumi.shintaku@riken.jp>
2021-11-02 12:42:01 +0900

    modified mapply with a "for" setence.

--- io/hunter_Seurat_load_adt_data.R
+++ io/hunter_Seurat_load_adt_data.R
@@ -1,10 +1,10 @@
 load.adt <- function (indexfiles,batch,channel) {
   rowend <- length(channel)+1
-  adt.xlsx <- read.xlsx(indexfiles[1],sheet=1, rows=16:111,cols=1:rowend,colNames = FALSE,rowNames = TRUE)
+  adt.xlsx <- read.xlsx(indexfiles,sheet=1, rows=16:111,cols=1:rowend,colNames = FALSE,rowNames = TRUE)
   colnames(adt.xlsx)<- channel#c("Events","FSC","SSC","Venus","APC","mCherry")
   
   # create normalized GFP
-  adt.xlsx$normGFP <- (adt.xlsx$Venus/adt.xlsx$mCherry)
+  #adt.xlsx$normGFP <- (adt.xlsx$Venus/adt.xlsx$mCherry)
   
   
   wellid <- rownames(adt.xlsx)
@@ -16,9 +16,9 @@ load.adt <- function (indexfiles,batch,channel) {
     adt.xlsx$cell[ij] <- paste0(batch,"-",pindex,"-",rtindex)
   }
   rownames(adt.xlsx)<-adt.xlsx$cell
-  adt.xlsx <- t(adt.xlsx)
-  adt.xlsx <- adt.xlsx[-nrow(adt.xlsx),]
-  return(adt.xlsx)
+  #adt.xlsx <- t(adt.xlsx)
+  adt.xlsx <- adt.xlsx[,-ncol(adt.xlsx)]
+    return(adt.xlsx)
 }
 
 #load FACS index data file name is 8 char long.
@@ -27,11 +27,22 @@ colnames(files)<-"name"
 batch <- unique(substr(files$name,1,8))
 indexfiles <- file.path(indexdir,paste0(batch,".xlsx"))
 channels <- rep(list(channel),length(indexfiles))
-adt.xlsx <- mapply(load.adt,indexfiles,batch,channels)
-adt.csv <- do.call("cbind",adt.xlsx)
+
+for (icnt in 1:length(indexfiles)){
+  adt.xlsx <- load.adt(indexfiles[icnt],batch[icnt],channels[[icnt]])
+  if (icnt==1){
+    adt.csv<-adt.xlsx
+  }else{
+    adt.csv<-rbind(adt.csv,adt.xlsx)
+  }
+}
+#adt.xlsx <- mapply(load.adt,indexfiles,batch,channels)
+
+#adt.csv <- do.call("cbind",adt.xlsx)
 
 # adding adt data
-seladt.csv <- adt.csv[,cellids] # extract cells detected in RNA-seq
+seladt.csv <- adt.csv[cellids,] # extract cells detected in RNA-seq
+
 seladt.csv[is.na(seladt.csv)] <- 0
 pbmc.adt <-as.sparse(seladt.csv) # convert the format to sparse matrix
 pbmc[["ADT"]] <- CreateAssayObject(counts=pbmc.adt)
