5a6c42c6ba9bdc2c8246ef0e72115bedb80d422a
hshintaku <hirofumi.shintaku@riken.jp>
2021-12-28 17:04:31 +0900

    DEseq2

--- bulk_DESeq2.R
+++ bulk_DESeq2.R
@@ -2,7 +2,6 @@ library(DESeq2)
 #onchip <- subset(islet,subset=condition=="minus",invert=TRUE)
 #onchip <- subset(onchip,subset=condition=="control",invert=TRUE)
 pbmc12.data <- islet[["RNA"]]@counts
-#CellScatter(pbmc,cell1="Ida-Lib-01-7",cell2="Ida-Lib-02-9")+scale_x_log10()+scale_y_log10()
 
 pbmc.mtx <- as.matrix(pbmc12.data)
 group <- data.frame(con = islet[["pool"]])
