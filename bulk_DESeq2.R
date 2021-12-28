c9c39f36e9d5532084eaa0882ca53c13c4ea4a15
hshintaku <hirofumi.shintaku@riken.jp>
2021-12-28 14:04:09 +0900

    remove WGCNA modules

--- bulk_DESeq2.R
+++ bulk_DESeq2.R
@@ -1,18 +1,25 @@
-
 library(DESeq2)
-pbmc.data <- Read10X(data.dir = wdir)
-pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 1, min.features = 100)
-pbmc12 <- subset(pbmc, subset = batch =="03", invert = TRUE)
 
-pbmc12.data <- data.frame(pbmc12[["RNA"]]@counts)
+piezo[["genotype"]] <- c("ko4ohm","dmso","ko4ohu","dmso","","test","test","test","test2","test2","test2","test2","test2","test2","test2","test2")
+
+pbmc12 <- subset(piezo,subset=genotype=="test",invert=TRUE)
+test1 <- subset(piezo,subset=genotype=="test1")
+test2 <- subset(piezo,subset=genotype=="test2")
+all <- merge(test1,y=test2, project = "hirano")
+
+
+
+pbmc12.data <- pbmc12[["RNA"]]@counts
 #CellScatter(pbmc,cell1="Ida-Lib-01-7",cell2="Ida-Lib-02-9")+scale_x_log10()+scale_y_log10()
 
 pbmc.mtx <- as.matrix(pbmc12.data)
-group <- data.frame(con = pbmc12[["batch"]])
+group <- data.frame(con = pbmc12[["genotype"]])
+
 #colnames(pbmc.mtx) <- group$batch
 
-dds <- DESeqDataSetFromMatrix(countData = pbmc.mtx, colData = group, design = ~ batch)
+dds <- DESeqDataSetFromMatrix(countData = pbmc.mtx, colData = group, design = ~ genotype)
 dds <- DESeq(dds)
 res <- results(dds)
+View(data.frame(res))
 head(res)
-plotMA(res, alpha = 0.01)
+plotMA(res, alpha = 1)
--- hunter_main.R
+++ hunter_main.R
@@ -35,52 +35,11 @@ source(file.path(rdir,"hunter_first_data_process.R"))
 # load data from 10x formatted files
 source(file.path(rdir,"hunter_Seurat_load_dataset.R"))
 pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 1e5)
-pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 200)
-#
-# create reference table with gene_short_name
-# source(file.path(rdir,'hunter_biomart_ref.R'))
-# gene_list <- data.frame(rownames(pbmc))
-# colnames(gene_list) <- "gene"
-# #hs_ref <- func.biomart.ref(hs_mart,gene_list,"hgnc_symbol")
-# filter="mgi_symbol"
-# symbol="mgi_symbol"
-# ms_ref <- unique(func.biomart.ref(ms_mart,gene_list,filter,symbol))
-# missing_ref <- subset(gene_list,!(gene %in% ms_ref$gene_short_name))
-# adding_ref <- data.frame(cbind(missing_ref$gene,missing_ref$gene,missing_ref$gene,missing_ref$gene,missing_ref$gene))
-# colnames(adding_ref) <- colnames(ms_ref)
-# rownames(adding_ref) <- adding_ref$ensembl_gene_id
-# ms_ref <- rbind(adding_ref,ms_ref)
-
-
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 1000)
 # technical check, pca and umap clustering for cell typing
-source(file.path(rdir,'shiomi_Seurat_technicalcheck.R'))
-
-# load FCS data
-#indexdir =paste0(wdir,"index/")
-indexdir <- "/home/samba/storage0/Shiomi/hunterindex"
-channel <- c("Events","FSC","SSC","Venus","mCherry")
-#c("Events","FSC","SSC","Venus","APC","mCherry")
-
-source(file.path(rdir,'io/hunter_Seurat_load_adt_data.R'))
-#
-# load cite-seq-count=FLD data
-#
-# preprocess FLD data
-source(file.path(rdir,"preprocess/preprocess_FLD_data.R"))
-source(file.path(rdir,'io/hunter_Seurat_load_fld_data.R'))
-source(file.path(rdir,"shiomi_fld_external_control_analysis.R"))
-# first overview
-# analyze data with PCA and UMAP
-# find clusters and marker genes
-#source(file.path(rdir,"shiomi_Seurat_technicalcheck.R"))
-
-# check cell cycle dependence 
-source(file.path(rdir,"shiomi_Seurat_cellcycle_dependence.R"))
-# find marker genes
-source(file.path(rdir,"shiomi_Seurat_Marker.genes.R"))
+source(file.path(rdir,'nashi_Seurat_clustering.R'))
 # compute pseudotime and order cells along the gene expression
 source(file.path(rdir,"shiomi_Seurat_monocle_pseudotime.R"))
-
 #
 # http://yulab-smu.top/clusterProfiler-book/index.html
 #
