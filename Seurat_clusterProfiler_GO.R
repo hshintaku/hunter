bbfb98b8e725f0c720d6aec528765a27128d318b
hshintaku <hirofumi.shintaku@riken.jp>
2022-01-24 16:12:55 +0900

    entrez from biomaRt

--- Seurat_clusterProfiler_GO.R
+++ Seurat_clusterProfiler_GO.R
@@ -3,24 +3,21 @@
 #BiocManager::install("org.Mm.eg.db")
 #library("org.Mm.eg.db")
 #BiocManager::install("org.Hs.eg.db")
-library("org.Hs.eg.db")
-library("org.Rn.eg.db")
 library(clusterProfiler)
 #
-ensg <- data.frame(unlist(as.list(org.Hs.egENSEMBL2EG)))
-ensrg <- data.frame(unlist(as.list(org.Rn.egENSEMBL2EG)))
-colnames(ensg)<-"gene"
-colnames(ensrg)<-"gene"
-# entrez annotation
-#ms_ref$entrez_annotation <- ensg[ms_ref$ensembl_gene_id,]
-#ms_ref[ms_ref$ensembl_gene_id,]$entrez_annotation <- ensrg[ms_ref$ensembl_gene_id,]
-en_ref <- rbind(ensg,ensrg)
-# add entrez_annotation
-ms_ref$entrez_annotation<-en_ref[ms_ref$ensembl_gene_id,]
+#library("org.Hs.eg.db")
+#library("org.Rn.eg.db")
+#ensg <- data.frame(unlist(as.list(org.Hs.egENSEMBL2EG)))
+#ensrg <- data.frame(unlist(as.list(org.Rn.egENSEMBL2EG)))
+#colnames(ensg)<-"gene"
+#colnames(ensrg)<-"gene"
+#en_ref <- rbind(ensg,ensrg)
+#ms_ref$entrezgene_id<-en_ref[ms_ref$ensembl_gene_id,]
+
 # remove genes not found in entrez
-ms_ref<-ms_ref[!is.na(ms_ref$entrez_annotation),]
+ms_ref<-ms_ref[!is.na(ms_ref$entrezgene_id),]
 # de_genes_enteez
-de_genes_entrez <- ms_ref[ms_ref$gene_short_name %in% rownames(de_genes),]$entrez_annotation
+de_genes_entrez <- ms_ref[ms_ref$gene_short_name %in% rownames(de_genes),]$entrezgene_id
 #
 # https://bioc.ism.ac.jp/packages/3.3/bioc/vignettes/clusterProfiler/inst/doc/clusterProfiler.html
 #
@@ -33,7 +30,7 @@ ego_result <- enrichGO(gene          = de_genes_entrez,
                        ont           = "CC",
                          pAdjustMethod = "BH",
                        pvalueCutoff  = 0.05,
-                       qvalueCutoff  = 0.05, 
+                       qvalueCutoff  = 0.1, 
                        readable      = TRUE)
 head(as.data.frame(ego_result))
 ego_result.simple<-simplify(ego_result)
